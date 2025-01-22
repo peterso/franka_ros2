fb006ef426153af848279692318271f4f34e9788
Peter So <petermjso@gmail.com>
2025-01-22 14:54:07 +0100

    add modified move_to_start for real2sim demo with mujoco

---
+++ franka_example_controllers/include/franka_example_controllers/move_to_start_multiverse_controller.hpp
@@ -0,0 +1,112 @@
+// Copyright (c) 2023 Franka Robotics GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <memory>
+#include <string>
+
+#include <Eigen/Eigen>
+#include <controller_interface/controller_interface.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+#include "motion_generator.hpp"
+
+#include "multiverse_client_json.h"
+
+using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+
+namespace franka_example_controllers {
+
+/// The move to start example controller moves the robot into default pose.
+class MoveToStartMultiverseController : public controller_interface::ControllerInterface,
+                                           public MultiverseClientJson{
+ public:
+  using Vector7d = Eigen::Matrix<double, 7, 1>;
+  [[nodiscard]] controller_interface::InterfaceConfiguration command_interface_configuration()
+      const override;
+  [[nodiscard]] controller_interface::InterfaceConfiguration state_interface_configuration()
+      const override;
+  controller_interface::return_type update(const rclcpp::Time& time,
+                                           const rclcpp::Duration& period) override;
+  CallbackReturn on_init() override;
+  CallbackReturn on_configure(const rclcpp_lifecycle::State& previous_state) override;
+  CallbackReturn on_activate(const rclcpp_lifecycle::State& previous_state) override;
+
+ private:
+  std::string arm_id_;
+  const int num_joints = 7;
+  Vector7d q_;
+  Vector7d q_goal_;
+  Vector7d dq_;
+  Vector7d dq_filtered_;
+  Vector7d k_gains_;
+  Vector7d d_gains_;
+  rclcpp::Time start_time_;
+  std::unique_ptr<MotionGenerator> motion_generator_;
+
+  void updateJointStates();
+
+ private:
+  void start_connect_to_server_thread() override;
+
+  void wait_for_connect_to_server_thread_finish() override;
+
+  void start_meta_data_thread() override;
+
+  void wait_for_meta_data_thread_finish() override;
+
+  bool init_objects(bool from_request_meta_data = false) override;
+
+  void bind_request_meta_data() override;
+
+  void bind_response_meta_data() override;
+
+  void bind_api_callbacks() override;
+
+  void bind_api_callbacks_response() override;
+
+  void init_send_and_receive_data() override;
+
+  void bind_send_data() override;
+
+  void bind_receive_data() override;
+
+  void clean_up() override;
+
+  void reset() override;
+
+ private:
+  std::map<std::string, std::string> meta_data;
+
+  std::map<std::string, std::set<std::string>> send_objects;
+
+  std::vector<double *> send_data_vec;
+
+  std::unordered_map<std::string, std::vector<std::string>> joint_interfaces = {
+      {"position", {}},
+      {"velocity", {}},
+      {"acceleration", {}}};
+
+  std::map<std::string, int> actuators;
+
+  std::map<int, double> init_joint_commands;
+
+  std::map<int, double*> joint_commands;
+
+  double sim_start_time;
+
+  std::thread commnunicate_thread;
+};
+}  // namespace franka_example_controllers
--- franka_example_controllers/src/move_to_start_multiverse_controller.cpp
+++ franka_example_controllers/src/move_to_start_multiverse_controller.cpp
@@ -12,20 +12,19 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <franka_example_controllers/joint_impedance_multiverse_controller.hpp>
-#include <franka_example_controllers/robot_utils.hpp>
+#include <franka_example_controllers/move_to_start_multiverse_controller.hpp>
 
 #include <cassert>
 #include <cmath>
 #include <exception>
-#include <string>
 
 #include <Eigen/Eigen>
+#include <controller_interface/controller_interface.hpp>
 
 namespace franka_example_controllers {
 
 controller_interface::InterfaceConfiguration
-JointImpedanceMultiverseController::command_interface_configuration() const {
+MoveToStartMultiverseController::command_interface_configuration() const {
   controller_interface::InterfaceConfiguration config;
   config.type = controller_interface::interface_configuration_type::INDIVIDUAL;
 
@@ -36,7 +35,7 @@ JointImpedanceMultiverseController::command_interface_configuration() const {
 }
 
 controller_interface::InterfaceConfiguration
-JointImpedanceMultiverseController::state_interface_configuration() const {
+MoveToStartMultiverseController::state_interface_configuration() const {
   controller_interface::InterfaceConfiguration config;
   config.type = controller_interface::interface_configuration_type::INDIVIDUAL;
   for (int i = 1; i <= num_joints; ++i) {
@@ -46,44 +45,54 @@ JointImpedanceMultiverseController::state_interface_configuration() const {
   return config;
 }
 
-controller_interface::return_type JointImpedanceMultiverseController::update(
+controller_interface::return_type MoveToStartMultiverseController::update(
     const rclcpp::Time& /*time*/,
-    const rclcpp::Duration& period) {
+    const rclcpp::Duration& /*period*/) {
 
   if (num_joints != 7) {
     const std::string error_log = "There are " + std::to_string(num_joints) + " joints.";
     RCLCPP_INFO(get_node()->get_logger(), error_log.c_str());
     return controller_interface::return_type::OK;
   }
-  if (receive_data_vec.size() != 7) {
-    const std::string error_log = "There are " + std::to_string(receive_data_vec.size()) + " data.";
+  if (send_data_vec.size() != 7) {
+    const std::string error_log = "There are " + std::to_string(send_data_vec.size()) + " data.";
     RCLCPP_INFO(get_node()->get_logger(), error_log.c_str());
     return controller_interface::return_type::OK;
   }
 
   updateJointStates();
+  auto trajectory_time = this->get_node()->now() - start_time_;
+  auto motion_generator_output = motion_generator_->getDesiredJointPositions(trajectory_time);
+  Vector7d q_desired = motion_generator_output.first;
+  bool finished = motion_generator_output.second;
+  if (not finished) {
+    const double kAlpha = 0.99;
+    dq_filtered_ = (1 - kAlpha) * dq_filtered_ + kAlpha * dq_;
+    Vector7d tau_d_calculated =
+        k_gains_.cwiseProduct(q_desired - q_) + d_gains_.cwiseProduct(-dq_filtered_);
+    for (int i = 0; i < 7; ++i) {
+      command_interfaces_[i].set_value(tau_d_calculated(i));
+    }
+  } else {
+    for (auto& command_interface : command_interfaces_) {
+      command_interface.set_value(0);
+    }
+    this->get_node()->set_parameter({"process_finished", true});
+  }
 
-  Vector7d q_goal = initial_q_;
   for (int i = 0; i < num_joints; i++)
   {
-    joint_states[i][0] = q_(i);
-    joint_states[i][1] = dq_(i);
-    q_goal(i) = joint_commands[i][0];
+    joint_commands[i][0] = q_(i);
   }
 
-  const double kAlpha = 0.99;
-  dq_filtered_ = (1 - kAlpha) * dq_filtered_ + kAlpha * dq_;
-  Vector7d tau_d_calculated =
-      k_gains_.cwiseProduct(q_goal - q_) + d_gains_.cwiseProduct(-dq_filtered_);
-  for (int i = 0; i < num_joints; ++i) {
-    command_interfaces_[i].set_value(tau_d_calculated(i));
-  }
   return controller_interface::return_type::OK;
 }
 
-CallbackReturn JointImpedanceMultiverseController::on_init() {
+CallbackReturn MoveToStartMultiverseController::on_init() {
+  q_goal_ << 0, -M_PI_4, 0, -3 * M_PI_4, 0, M_PI_2, M_PI_4;
   try {
-    auto_declare<std::string>("arm_id", "");
+    auto_declare<bool>("process_finished", false);
+    auto_declare<std::string>("arm_id", "fr3");
     auto_declare<std::vector<double>>("k_gains", {});
     auto_declare<std::vector<double>>("d_gains", {});
   } catch (const std::exception& e) {
@@ -112,18 +121,12 @@ CallbackReturn JointImpedanceMultiverseController::on_init() {
   server_port = "7000";
   client_port = "1111";
 
-  std::vector<double> init_joint_values = {0, -M_PI_4, 0, -3 * M_PI_4, 0, M_PI_2, M_PI_4};
-  for (int i = 0; i < 7; i++) {
-    const std::string joint_name = "real_joint" + std::to_string(i+1);
-    send_objects[joint_name] = {"joint_rvalue", "joint_angular_velocity"};
-    joints[joint_name] = i;
-    joint_states[i] = (double*)calloc(2, sizeof(double));
-    
-    const std::string actuator_name = "actuator" + std::to_string(i+1);
-    receive_objects[actuator_name] = {"cmd_joint_rvalue"};
+  for (int i = 0; i < 7; i++) {    
+    const std::string actuator_name = "panda_joint" + std::to_string(i+1);
+    send_objects[actuator_name] = {"joint_rvalue"};
     actuators[actuator_name] = i;
     
-    init_joint_commands[i] = init_joint_values[i];
+    init_joint_commands[i] = q_goal_[i];
     joint_commands[i] = (double*)calloc(3, sizeof(double));
     joint_commands[i][0] = init_joint_commands[i];
   }
@@ -149,7 +152,7 @@ CallbackReturn JointImpedanceMultiverseController::on_init() {
   return CallbackReturn::SUCCESS;
 }
 
-CallbackReturn JointImpedanceMultiverseController::on_configure(
+CallbackReturn MoveToStartMultiverseController::on_configure(
     const rclcpp_lifecycle::State& /*previous_state*/) {
   arm_id_ = get_node()->get_parameter("arm_id").as_string();
   auto k_gains = get_node()->get_parameter("k_gains").as_double_array();
@@ -177,35 +180,18 @@ CallbackReturn JointImpedanceMultiverseController::on_configure(
     k_gains_(i) = k_gains.at(i);
   }
   dq_filtered_.setZero();
-
-  auto parameters_client =
-      std::make_shared<rclcpp::AsyncParametersClient>(get_node(), "/robot_state_publisher");
-  parameters_client->wait_for_service();
-
-  auto future = parameters_client->get_parameters({"robot_description"});
-  auto result = future.get();
-  if (!result.empty()) {
-    robot_description_ = result[0].value_to_string();
-  } else {
-    RCLCPP_ERROR(get_node()->get_logger(), "Failed to get robot_description parameter.");
-  }
-
-  arm_id_ = robot_utils::getRobotNameFromDescription(robot_description_, get_node()->get_logger());
-
   return CallbackReturn::SUCCESS;
 }
 
-CallbackReturn JointImpedanceMultiverseController::on_activate(
+CallbackReturn MoveToStartMultiverseController::on_activate(
     const rclcpp_lifecycle::State& /*previous_state*/) {
   updateJointStates();
-  dq_filtered_.setZero();
-  initial_q_ = q_;
-  elapsed_time_ = 0.0;
-
+  motion_generator_ = std::make_unique<MotionGenerator>(0.2, q_, q_goal_);
+  start_time_ = this->get_node()->now();
   return CallbackReturn::SUCCESS;
 }
 
-void JointImpedanceMultiverseController::updateJointStates() {
+void MoveToStartMultiverseController::updateJointStates() {
   for (auto i = 0; i < num_joints; ++i) {
     const auto& position_interface = state_interfaces_.at(2 * i);
     const auto& velocity_interface = state_interfaces_.at(2 * i + 1);
@@ -218,23 +204,23 @@ void JointImpedanceMultiverseController::updateJointStates() {
   }
 }
 
-bool JointImpedanceMultiverseController::init_objects(bool) {
-  return send_objects.size() > 0 && receive_objects.size() > 0;
+bool MoveToStartMultiverseController::init_objects(bool) {
+  return send_objects.size() > 0;
 }
 
-void JointImpedanceMultiverseController::start_connect_to_server_thread() {
+void MoveToStartMultiverseController::start_connect_to_server_thread() {
   connect_to_server();
 }
 
-void JointImpedanceMultiverseController::wait_for_connect_to_server_thread_finish() {}
+void MoveToStartMultiverseController::wait_for_connect_to_server_thread_finish() {}
 
-void JointImpedanceMultiverseController::start_meta_data_thread() {
+void MoveToStartMultiverseController::start_meta_data_thread() {
   send_and_receive_meta_data();
 }
 
-void JointImpedanceMultiverseController::wait_for_meta_data_thread_finish() {}
+void MoveToStartMultiverseController::wait_for_meta_data_thread_finish() {}
 
-void JointImpedanceMultiverseController::bind_request_meta_data() {
+void MoveToStartMultiverseController::bind_request_meta_data() {
   // Create JSON object and populate it
   request_meta_data_json.clear();
   request_meta_data_json["meta_data"]["world_name"] = meta_data["world_name"];
@@ -251,83 +237,59 @@ void JointImpedanceMultiverseController::bind_request_meta_data() {
     }
   }
 
-  for (const std::pair<const std::string, std::set<std::string>>& receive_object :
-       receive_objects) {
-    for (const std::string& attribute_name : receive_object.second) {
-      request_meta_data_json["receive"][receive_object.first].append(attribute_name);
-    }
-  }
-
   request_meta_data_str = request_meta_data_json.toStyledString();
   RCLCPP_INFO(get_node()->get_logger(), request_meta_data_str.c_str());
 }
 
-void JointImpedanceMultiverseController::bind_response_meta_data() {
+void MoveToStartMultiverseController::bind_response_meta_data() {
   RCLCPP_INFO(get_node()->get_logger(), response_meta_data_str.c_str());
 }
 
-void JointImpedanceMultiverseController::bind_api_callbacks() {}
+void MoveToStartMultiverseController::bind_api_callbacks() {}
 
-void JointImpedanceMultiverseController::bind_api_callbacks_response() {}
+void MoveToStartMultiverseController::bind_api_callbacks_response() {}
 
-void JointImpedanceMultiverseController::init_send_and_receive_data() {
+void MoveToStartMultiverseController::init_send_and_receive_data() {
   for (const std::pair<const std::string, std::set<std::string>>& send_object :
        send_objects) {
-    const int i = joints[send_object.first];
+    const int i = actuators[send_object.first];
     for (const std::string& attribute_name : send_object.second) {
-      if (strcmp(attribute_name.c_str(), "joint_tvalue") == 0 ||
-          strcmp(attribute_name.c_str(), "joint_rvalue") == 0) {
-        send_data_vec.emplace_back(&joint_states.at(i)[0]);
-      } else if (strcmp(attribute_name.c_str(), "joint_linear_velocity") == 0 ||
-                 strcmp(attribute_name.c_str(), "joint_angular_velocity") == 0) {
-        send_data_vec.emplace_back(&joint_states.at(i)[0]);
-      }
-    }
-  }
-  for (const std::pair<const std::string, std::set<std::string>>& receive_object :
-       receive_objects) {
-    const int i = actuators[receive_object.first];
-    for (const std::string& attribute_name : receive_object.second) {
       if (strcmp(attribute_name.c_str(), "cmd_joint_tvalue") == 0 ||
-          strcmp(attribute_name.c_str(), "cmd_joint_rvalue") == 0) {
-        receive_data_vec.emplace_back(&joint_commands.at(i)[0]);
+          strcmp(attribute_name.c_str(), "joint_rvalue") == 0) {
+        send_data_vec.emplace_back(&joint_commands.at(i)[0]);
       } else if (strcmp(attribute_name.c_str(), "cmd_joint_linear_velocity") == 0 ||
                  strcmp(attribute_name.c_str(), "cmd_joint_angular_velocity") == 0) {
-        receive_data_vec.emplace_back(&joint_commands.at(i)[1]);
+        send_data_vec.emplace_back(&joint_commands.at(i)[0]);
       }
     }
   }
 }
 
-void JointImpedanceMultiverseController::bind_send_data() {
+void MoveToStartMultiverseController::bind_send_data() {
   *world_time = get_time_now() - sim_start_time;
   for (size_t i = 0; i < send_buffer.buffer_double.size; i++) {
     send_buffer.buffer_double.data[i] = *send_data_vec[i];
   }
 }
 
-void JointImpedanceMultiverseController::bind_receive_data() {
-  for (size_t i = 0; i < receive_buffer.buffer_double.size; i++) {
-    *receive_data_vec[i] = receive_buffer.buffer_double.data[i];
-  }
+void MoveToStartMultiverseController::bind_receive_data() {
+
 }
 
-void JointImpedanceMultiverseController::clean_up() {
+void MoveToStartMultiverseController::clean_up() {
   send_data_vec.clear();
-  receive_data_vec.clear();
 }
 
-void JointImpedanceMultiverseController::reset() {
+void MoveToStartMultiverseController::reset() {
   sim_start_time = get_time_now();
   for (int i = 0; i < num_joints; i++) {
-    joint_commands[i][0] = initial_q_(i);
+    joint_commands[i][0] = init_joint_commands[i];
     joint_commands[i][1] = 0.0;
     joint_commands[i][2] = 0.0;
   }
 }
-
 }  // namespace franka_example_controllers
 #include "pluginlib/class_list_macros.hpp"
 // NOLINTNEXTLINE
-PLUGINLIB_EXPORT_CLASS(franka_example_controllers::JointImpedanceMultiverseController,
+PLUGINLIB_EXPORT_CLASS(franka_example_controllers::MoveToStartMultiverseController,
                        controller_interface::ControllerInterface)
